6ed28582b8e0f2ddbd9ffc7af19c3915afff0ef9
joavillegas <joaquinvillegas241@gmail.com>
2022-10-12 17:28:57 -0300

    modificación comoponente WrongPage y adición de estilos correspondientes - Joaquin Villegas

--- src/Componentes/WrongPage.js
+++ src/Componentes/WrongPage.js
@@ -1,8 +1,17 @@
+import img  from '../Assets/Icon/error404.png';
+import "./Styles/WrongPage.css"
+
+
+
 
 export default function WrongPage() {
-    
+
     return (
         <div className="contain-wrongPage">
+            <img
+                src={img}
+                className="imgerror"
+            />
             <h1>Error 404</h1>
             <h2>Not Found</h2>
             <h3> The resources requested could not be found on this server </h3>
